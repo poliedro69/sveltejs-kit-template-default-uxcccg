0c756e87b43e0b4a973b4667bb61a570507ffdda
poliedro69 <46156033+poliedro69@users.noreply.github.com>
2022-11-07 11:09:47 +0100

    Initial commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<script>
+    import tools from '/src/tools.json';
+</script>
+
+<div class="text-column">
+	<h1>About this app</h1>
+	{#each tools as tool}
+		<p>{tool.id}</p>
+	{/each}
+</div>
